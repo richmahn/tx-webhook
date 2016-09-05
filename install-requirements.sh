b0dcc2ec4fdfbfd8b571d0b6fb4ddce1c2b36641
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-09-05 12:25:15 -0600

    Fixed install-requirements.sh

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-LATEST=$(curl -s https://api.github.com/repos/apex/apex/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
-URL="https://github.com/apex/apex/releases/download/${LATEST}/apex_linux_amd64"
-
-curl -sL ${URL} -o ./apex
-chmod +x ./apex
+for file in `ls functions/*/requirements.txt`
+do
+  pip install -r $file -t `dirname $file` 
+done
